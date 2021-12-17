b4d1ac0f16702c29ee559028812ba1fb1ed8a3d1
dchaitu <chaitanyadokara@gmail.com>
2021-12-17 17:00:55 +0530

    Added Links for all tabs/pages

--- lib/navigation/app_link.dart
+++ lib/navigation/app_link.dart
@@ -21,7 +21,6 @@ class AppLink {
     this.itemId,
   });
 
-// TODO: Add fromLocation
   static AppLink fromLocation(String? location) {
     // 1
     location = Uri.decodeFull(location ?? '');
@@ -42,6 +41,42 @@ class AppLink {
     // 6
     return link;
   }
-// TODO: Add toLocation
+
+  String toLocation() {
+    // 1
+    String addKeyValPair({
+      required String key,
+      String? value,
+    }) =>
+        value == null ? '' : '${key}=$value&';
+    // 2
+    switch (location) {
+    // 3
+      case loginPath:
+        return loginPath;
+    // 4
+      case onboardingPath:
+        return onboardingPath;
+    // 5
+      case profilePath:
+        return profilePath;
+    // 6
+      case itemPath:
+        var loc = '$itemPath?';
+        loc += addKeyValPair(
+          key: idParam,
+          value: itemId,
+        );
+        return Uri.encodeFull(loc);
+    // 7
+      default:
+        var loc = '$homePath?';
+        loc += addKeyValPair(
+          key: tabParam,
+          value: currentTab.toString(),
+        );
+        return Uri.encodeFull(loc);
+    }
+  }
 
 }
